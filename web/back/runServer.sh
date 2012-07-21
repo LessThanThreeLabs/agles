197f9bf471795185a336684eace305442a450bad
Jordan Potter <JordanNPotter@gmail.com>
2012-07-21 04:14:54 -0700

    Disallows running runServer.sh from another directory

--- web/back/runServer.sh
+++ web/back/runServer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if [ -d 'js' ]; then
+
+if [ "$(dirname $0)" != "." ]; then
+	echo 'You must run this script from its directory'
+elif [ -d 'js' ]; then
 	node js/index.js
 else
 	echo 'You must first compile coffeescript into javascript by running compile.sh'
