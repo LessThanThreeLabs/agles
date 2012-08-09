5fd42a0a6b89e096ff52b1d252b9875f3714b298
Jonathan Chu <jchonphoenix@gmail.com>
2012-08-08 22:19:19 -0700

    Rewrote FileSystemRepoManager
    Slight changes in architecture. Each repo now is associated with a
    unique hash identifier, which is used to determine where on the
    filesystem this repository should go. FileSystemRepoManager now accounts
    for the repo hash.

--- git/repo_manager.py
+++ git/repo_manager.py
@@ -11,51 +11,76 @@ a git implementation written in python.
 import os
 import shutil
 
+from os import sep
 from dulwich.repo import Repo
 
-class RepoManager(object):
+class FileSystemRepoManager(object):
 	"""Management class for server side git repositories"""
+
+	DIR_LEVELS = 3
 	
 	def __init__(self):
 		pass
 
-	def create(self, repo_name):
+	def create(self, repo_hash, repo_name):
 		"""Creates a new server side repository.
 		We create bare repositories because they are server side.
 
+		:param repo_hash: A unique hash assigned to each repository that determines which directory
+						  the repository is stored under.
 		:param repo_name: The name of the new repository.
 		:return: The newly created repository.
 		"""
 
-		# TODO: figure out where/how we are storing repositories
-		if not os.path.exists(repo_name):
-			os.mkdir(repo_name)
+		repo_path = self._resolve_path(repo_hash, repo_name)
+		if not os.path.exists(repo_path):
+			os.makedirs(repo_path)
 		else:
-			raise RepositoryAlreadyExistsException(repo_name)
-		repo = Repo.init_bare(repo_name)
+			raise RepositoryAlreadyExistsException(repo_hash, repo_path)
+		repo = Repo.init_bare(repo_path)
 		return repo
 
-	def delete(self, repo_name):
+	def delete(self, repo_hash, repo_name):
 		"""Deletes a server side repository. This cannot be undone.
 
+		:param repo_hash: A unique hash assigned to each repository that determines which directory
+						  the repository is stored under.
 		:param repo_name: The name of the repository to be deleted.
 		"""
-
-		shutil.rmtree(repo_name)
+		repo_path = self._resolve_path(repo_hash, repo_name)
+		shutil.rmtree(repo_path)
 
 	def rename(self, old_name, new_name):
 		"""Renames a repository.
 
+		:param repo_hash: A unique hash assigned to each repository that determines which directory
+						  the repository is stored under.
 		:param old_name: The old repository name.
 		:param new_name: The new repository name.
 		:return: The renamed repository.
 		"""
 
-		if not os.path.exists(new_name):
-			shutil.move(old_name, new_name)
+		old_repo_path = self._resolve_path(repo_hash, old_name)
+		new_repo_path = self._resolve_path(repo_hash, new_name)
+		if not os.path.exists(new_repo_path):
+			shutil.move(old_repo_path, new_repo_path)
 		else:
-			raise RepositoryAlreadyExistsException(new_name) 
-		return Repo(new_name)
+			raise RepositoryAlreadyExistsException(repo_hash, new_repo_path) 
+		return Repo(new_repo_path)
+
+	def _resolve_path(self, repo_hash, repo_name):
+		repo_path = sep.join([self._directory_treeify(repo_hash, self.DIR_LEVELS), repo_name])
+		return os.path.realpath(repo_path)
+
+	def _directory_treeify(self, repo_hash, dir_levels):
+		"""Takes a hash and separates it into directories (e.g. a23fe89 => a/2/3fe89)
+
+		:param repo_hash: The hash we are treeifying.
+		:param dir_levels: The number of directory levels to create from repo_hash.
+		:return: A string representing repo_hash with file separators up to dir_levels levels.
+		"""
+		return sep.join(repo_hash[:dir_levels]) + repo_hash[dir_levels:]
+
 
 class RepositoryManagementException(Exception):
 	"""Base class for exception relating to repository management."""
@@ -66,7 +91,7 @@ class RepositoryManagementException(Exception):
 class RepositoryAlreadyExistsException(RepositoryManagementException):
 	"""Indicates an exception occured due to a repository already existing."""
 
-	def __init__(self, existing_repo):
+	def __init__(self, repo_hash, existing_repo_path):
 		RepositoryManagementException.__init__(
 			self, 
-			'Repository %s already exists' % new_name)
+			'Repository with hash %s already exists at path %s' % (repo_hash, existing_repo_path))
