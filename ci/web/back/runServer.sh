cac5f5d2600c1f2ac2d1fbb9abfbdb585cf2b145
Jonathan Chu <jchonphoenix@gmail.com>
2012-11-04 12:13:05 -0800

    Fixing ordering, inconsistencies and pathing in runServer.sh

--- ci/web/back/runServer.sh
+++ ci/web/back/runServer.sh
@@ -4,9 +4,9 @@ if [ "$(dirname $0)" != "." ]; then
 	echo 'You must run this script from its directory'
 elif [ -d 'js' ]; then
 	mkdir -p logs/redis
+	redis-server conf/redis/sessionStoreRedis.conf &
+	redis-server conf/redis/createAccountRedis.conf &
 	node --harmony js/index.js &
-	redis-server sessionStoreRedis.conf &
-	redis-server createAccountRedis.conf
 else
 	echo 'You must first compile coffeescript into javascript by running compile.sh'
 fi
