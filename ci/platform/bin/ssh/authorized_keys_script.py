0ca71bbb43a7127cfaf9c3a2fe20aa2f1e668766
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-11-09 13:21:33 -0800

    Fixing authorized keys script, making gitserve executable

--- ci/platform/bin/ssh/authorized_keys_script.py
+++ ci/platform/bin/ssh/authorized_keys_script.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
+import os
 import re
 import sys
 
 from model_server import ModelServer
 
-FORCED_COMMAND = 'command="gitserve.py %d",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty'
+GITSERVE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "gitserve.py")
+FORCED_COMMAND = 'command="%s %d",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty'
 valid_key = re.compile('^ssh-(?:dss|rsa) [A-Za-z0-9+/]+$')
 
 try:
-	sshkey = sys.stdin.readline()
+	sshkey = sys.stdin.readline().rstrip()
 	if not valid_key.match(sshkey):
 		sys.exit(1)
 
@@ -16,9 +18,11 @@ try:
 		user_id = client.get_user_id_from_public_key(sshkey)
 
 	if user_id:
-		sys.stdout.write(FORCED_COMMAND % user_id)
+		sys.stdout.write(FORCED_COMMAND % (GITSERVE, user_id))
 		sys.exit(0)
 	else:
 		sys.exit(1)
+except SystemExit as e:
+	sys.exit(e)
 except:
 	sys.exit(1)
