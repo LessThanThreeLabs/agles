068acc51cea08be14f6e6108aa638afcdeb095c1
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-11-13 14:06:53 -0800

    Backgrounding server processes

--- ci/chef/cookbooks/agles/recipes/filesystem_repo_server.rb
+++ ci/chef/cookbooks/agles/recipes/filesystem_repo_server.rb
@@ -3,6 +3,6 @@ execute "Stop filesystem repo server" do
 end
 
 execute "Start filesystem repo server" do
-	command "#{node[:agles][:source_path][:internal]}/ci/platform/bin/start_filesystem_repo_server.py"
+	command "#{node[:agles][:source_path][:internal]}/ci/platform/bin/start_filesystem_repo_server.py &"
 	user "git"
 end
--- ci/chef/cookbooks/agles/recipes/model_server.rb
+++ ci/chef/cookbooks/agles/recipes/model_server.rb
@@ -3,6 +3,6 @@ execute "Stop model server" do
 end
 
 execute "Start model server" do
-	command "#{node[:agles][:source_path][:internal]}/ci/platform/bin/start_model_server.py"
+	command "#{node[:agles][:source_path][:internal]}/ci/platform/bin/start_model_server.py &"
 	user node[:agles][:user]
 end
