9ae15809b9b041dc73417971b812371821017891
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-11-14 22:57:39 -0800

    Reordering verification setup

--- ci/chef/roles/verification_box_run.rb
+++ ci/chef/roles/verification_box_run.rb
@@ -3,7 +3,7 @@ description "Stripped down agles verification virtual box run"
 run_list(
 	"recipe[agles::copy_source]",
 	"recipe[agles::language_config]",
-	"recipe[agles::setup_config]",
 	"recipe[agles::build_config]",
-	"recipe[agles::test_config]"
+	"recipe[agles::test_config]",
+	"recipe[agles::setup_config]"
 )
