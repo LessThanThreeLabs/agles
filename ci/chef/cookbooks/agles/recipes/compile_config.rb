c84cda94d5a20b14bf4edce6e01dc8b59ef58b32
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-11-15 23:18:33 -0800

    Bumping agles_config schema value, including renamed file

---
+++ ci/chef/cookbooks/agles/recipes/compile_config.rb
@@ -0,0 +1,34 @@
+include_recipe "agles"
+
+def create_compilescript(name, path, command)
+	template "/home/#{node[:agles][:user]}/scripts/compile/#{name}.sh" do
+		source "shell_command.erb"
+		owner node[:agles][:user]
+		mode "0755"
+		variables(
+			:user => node[:agles][:user],
+			:path => path,
+			:command => command
+		)
+	end
+end
+
+def handle_compiles(compiles)
+	compiles.each do |compile|
+		name = compile.first
+		config = compile.last
+		path = config["path"]
+		command = config["script"]
+		create_compilescript(name, path, command)
+	end
+end
+
+def handle_config(config)
+	handle_compiles(config["compile"]) if config.has_key? "compile"
+end
+
+config_path = "#{node[:agles][:source_path][:internal]}/#{node[:agles][:config_path]}"
+if File.exist? config_path
+	config = YAML::load(File.read(config_path))
+	handle_config config
+end
