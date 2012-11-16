54853585f823706d95978dae7633a74708fea2ad
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-11-15 18:33:44 -0800

    Adding gituser setup to chef

---
+++ ci/chef/cookbooks/agles/recipes/gituser.rb
@@ -0,0 +1,9 @@
+user "git" do
+	system true
+end
+
+directory "/git"
+
+directory "/git/repositories" do
+	owner "git"
+end
--- ci/chef/roles/repostore.rb
+++ ci/chef/roles/repostore.rb
@@ -11,6 +11,6 @@ override_attributes(
 			:platform => "/home/lt3/code/agles/ci/platform",
 			:authorized_keys_script => "/home/lt3/code/agles/ci/platform/bin/ssh/authorized_keys_script.py"
 		},
-		:repositories_path => "/tmp/repositories"
+		:repositories_path => "/git/repositories"
 	}
 )
