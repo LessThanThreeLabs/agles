c2f72094a3901a3d2d2bf1f2533fba5d23f1b855
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-11-22 21:54:06 -0500

    Fixing chef vagrant status checker

--- ci/chef/cookbooks/agles/recipes/verification_server.rb
+++ ci/chef/cookbooks/agles/recipes/verification_server.rb
@@ -19,7 +19,7 @@ node[:agles][:verification][:server_count].to_i.times do |server_num|
 		mkdir -p #{vagrant_path}
 		cd #{vagrant_path}
 		vagrant init precise64_verification
-		while [ "`vagrant status | grep default | sed -e 's/default\\s+\\(.*\\)/\\1/'`" != 'running' ]
+		while [ "`vagrant status | grep default | sed -e 's/default\\s\\+\\(.*\\)/\\1/'`" != 'running' ]
 			do vagrant destroy -f
 			vagrant up --no-provision
 		done
