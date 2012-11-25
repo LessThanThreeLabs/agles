953c08ebb18cb60d02ceb8624ae5b1ad0336c7f8
Jordan Potter <jordannpotter@gmail.com>
2012-11-24 19:39:49 -0800

    Removes stray character

--- ci/platform/bin/data_generator.py
+++ ci/platform/bin/data_generator.py
@@ -15,7 +15,7 @@ ADMIN_SALT = 'a'*16
 
 NUM_REPOS = 3
 REPOSITORIES_PATH = 'repos'
-VnALID_STATUSES = ['success', 'waiting', 'failed']
+VALID_STATUSES = ['success', 'waiting', 'failed']
 
 
 class SchemaDataGenerator(object):
