99155c1927e0002fb9fc366556c985869bda9cf1
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-11-26 18:25:07 -0800

    Adding missing update handler file

---
+++ ci/platform/model_server/users/update_handler.py
@@ -0,0 +1,14 @@
+from database import schema
+from database.engine import ConnectionFactory
+from model_server.rpc_handler import ModelServerRpcHandler
+
+
+class UsersUpdateHandler(ModelServerRpcHandler):
+	def __init__(self):
+		super(UsersUpdateHandler, self).__init__("users", "update")
+
+	def register_ssh_pubkey(self, user_id, alias, ssh_key):
+		ssh_pubkey = schema.ssh_pubkey
+		ins = ssh_pubkey.insert().values(user_id=user_id, alias=alias, ssh_key=ssh_key)
+		with ConnectionFactory.get_sql_connection() as sqlconn:
+			sqlconn.execute(ins)
