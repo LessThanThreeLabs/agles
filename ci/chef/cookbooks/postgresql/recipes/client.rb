023ecf5353e5ffca9d7cc4ba13260d735ba037f7
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-12-11 15:41:58 -0800

    Updating postgresql chef cookbook

--- ci/chef/cookbooks/postgresql/recipes/client.rb
+++ ci/chef/cookbooks/postgresql/recipes/client.rb
@@ -19,26 +19,12 @@
 # limitations under the License.
 #
 
-pg_packages = case node['platform']
-when "ubuntu","debian"
-  %w{postgresql-client libpq-dev make}
-when "fedora","suse","amazon"
-  %w{postgresql-devel}
-when "redhat","centos","scientific"
-  case
-  when node['platform_version'].to_f >= 6.0
-    %w{postgresql-devel}
-  else
-    [ "postgresql#{node['postgresql']['version'].split('.').join}-devel" ]
-  end
+if(node['postgresql']['enable_pitti_ppa'])
+  include_recipe 'postgresql::ppa_pitti_postgresql'
 end
 
-pg_packages.each do |pg_pack|
-  package pg_pack do
-    action :nothing
-  end.run_action(:install)
-end
+node['postgresql']['client']['packages'].each do |pg_pack|
 
-gem_package "pg" do
-  action :nothing
-end.run_action(:install)
+  package pg_pack
+
+end
