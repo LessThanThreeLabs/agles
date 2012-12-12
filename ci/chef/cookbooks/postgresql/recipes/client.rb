c1a80bcf007f1a1540443472ce324c302801aabb
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-12-11 17:51:53 -0800

    Reverting postgresql cookbook

--- ci/chef/cookbooks/postgresql/recipes/client.rb
+++ ci/chef/cookbooks/postgresql/recipes/client.rb
@@ -19,12 +19,26 @@
 # limitations under the License.
 #
 
-if(node['postgresql']['enable_pitti_ppa'])
-  include_recipe 'postgresql::ppa_pitti_postgresql'
+pg_packages = case node['platform']
+when "ubuntu","debian"
+  %w{postgresql-client libpq-dev make}
+when "fedora","suse","amazon"
+  %w{postgresql-devel}
+when "redhat","centos","scientific"
+  case
+  when node['platform_version'].to_f >= 6.0
+    %w{postgresql-devel}
+  else
+    [ "postgresql#{node['postgresql']['version'].split('.').join}-devel" ]
+  end
 end
 
-node['postgresql']['client']['packages'].each do |pg_pack|
-
-  package pg_pack
-
+pg_packages.each do |pg_pack|
+  package pg_pack do
+    action :nothing
+  end.run_action(:install)
 end
+
+gem_package "pg" do
+  action :nothing
+end.run_action(:install)
