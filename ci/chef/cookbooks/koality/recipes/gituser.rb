40f87499869d534f6712372805cd024cc943bb38
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-12-12 23:28:36 -0800

    Adding auto-sshkey generation for gituser, disable strict ssh host checking for forwarded command

--- ci/chef/cookbooks/koality/recipes/gituser.rb
+++ ci/chef/cookbooks/koality/recipes/gituser.rb
@@ -3,10 +3,20 @@ chef_gem "ruby-shadow"
 user "git" do
 	system true
 	password "$1$hiX0Dkyd$USCqEEofXmio5YPN7/NWE1"
-	home "/"
+	home "/home/git"
 end
 
 directory "/git/repositories" do
 	owner "git"
 	recursive true
+end
+
+bash "register own ssh key" do
+	cwd "/home/git"
+	user "git"
+	code <<-EOH
+		ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -C gituser
+		cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+		EOH
+	not_if {File.exists?("/home/git/.ssh/id_rsa")}
 end
