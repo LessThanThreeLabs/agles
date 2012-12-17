ed6d0b1c100a989100a10464329e0f9c7ce5b25a
Jonathan Chu <jchonphoenix@gmail.com>
2012-12-17 13:16:35 -0800

    Adding defaults to log.py so you can log to different files.

--- ci/platform/settings/log.py
+++ ci/platform/settings/log.py
@@ -5,14 +5,14 @@ import os
 LOG_HOME = './log'
 
 
-def configure():
+def configure(log_home=LOG_HOME, filepath='/default.log'):
 	# formatters
 	simple_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 	# create log directory
-	if not os.access(LOG_HOME, os.F_OK):
-		os.makedirs(LOG_HOME)
+	if not os.access(log_home, os.F_OK):
+		os.makedirs(log_home)
 	# handlers
-	default_handler = logging.handlers.RotatingFileHandler(filename=LOG_HOME + '/default.log', maxBytes=8388608, backupCount=4)
+	default_handler = logging.handlers.RotatingFileHandler(filename=log_home + filepath, maxBytes=8388608, backupCount=4)
 	default_handler.setFormatter(simple_formatter)
 	# loggers
 	root_logger = logging.getLogger()
