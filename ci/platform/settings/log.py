f00b3bb6342592e95d547395aa9943ac275e0cfc
Brian Bland <r4nd0m1n4t0r@gmail.com>
2012-12-17 09:59:26 -0800

    Fixing logging by creating log directories

--- ci/platform/settings/log.py
+++ ci/platform/settings/log.py
@@ -1,5 +1,6 @@
 import logging
 import logging.handlers
+import os
 
 LOG_HOME = './log'
 
@@ -7,6 +8,9 @@ LOG_HOME = './log'
 def configure():
 	# formatters
 	simple_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	# create log directory
+	if not os.access(LOG_HOME, os.F_OK):
+		os.makedirs(LOG_HOME)
 	# handlers
 	default_handler = logging.handlers.RotatingFileHandler(filename=LOG_HOME + '/default.log', maxBytes=8388608, backupCount=4)
 	default_handler.setFormatter(simple_formatter)
