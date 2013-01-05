db7e034a0b337e84af8f121d79b903c745157559
Jonathan Chu <jchonphoenix@gmail.com>
2013-01-05 13:56:43 -0800

    Fixing issues with platform redis.
    Moving the port to 6400. Also creates a conf file and puts the rdb files
    in production/db/redis

--- ci/chef/cookbooks/koality/recipes/filesystem_repo_server.rb
+++ ci/chef/cookbooks/koality/recipes/filesystem_repo_server.rb
@@ -7,9 +7,23 @@ supervisor_service "filesystem_repo_server" do
 	directory "/tmp/repo_server"
 end
 
+supervisor_service "redis-repostore" do
+	action [:stop]
+end
+
 directory "/tmp/repo_server" do
 	owner "git"
 	group "git"
+end
+
+supervisor_service "redis-repostore" do
+	action [:enable, :start]
+	command "redis-server #{node[:koality][:source_path][:internal]}/ci/platform/conf/redis/filesystem_repo_server_redis.conf"
+	stdout_logfile "#{node[:koality][:supervisor][:logdir]}/redis-repostore_stdout.log"
+	stderr_logfile "#{node[:koality][:supervisor][:logdir]}/redis-repostore_stderr.log"
+	directory "#{node[:koality][:source_path][:internal]}/ci/production"
+	user node[:koality][:user]
+	priority 0
 end
 
 supervisor_service "filesystem_repo_server" do
@@ -19,5 +33,5 @@ supervisor_service "filesystem_repo_server" do
 	stdout_logfile "#{node[:koality][:supervisor][:logdir]}/filesystem_repo_server_stdout.log"
 	stderr_logfile "#{node[:koality][:supervisor][:logdir]}/filesystem_repo_server_stderr.log"
 	user "git"
-	priority 0
+	priority 1
 end
