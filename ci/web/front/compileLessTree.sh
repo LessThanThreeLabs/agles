1df9e9ba8e4000e051bcdb1fc104c0d2a06e0fd9
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-01-31 20:16:15 -0800

    Cleaning up web compile scripts a bit

--- ci/web/front/compileLessTree.sh
+++ ci/web/front/compileLessTree.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
-failures=0
 declare -a pids=()
 declare -a lessfiles=()
 while read filename; do
-	cssname=$(basename $filename .less)".css"
-	outdir=$(echo $(dirname $filename) | sed s:/src/:/css/src/: | sed s:^src/:css/src/: | sed s:^src$:css/src:)
-	if [ ! -d $outdir ]; then
-		mkdir -p $outdir
-	fi
-	outfilename=$outdir/$cssname
-	lessc $filename $outfilename &
+	(	cssname=$(basename $filename .less).css
+		outdir=$(echo $(dirname $filename) | sed s:/src/:/css/src/: | sed s:^src/:css/src/: | sed s:^src$:css/src:)
+		if [ ! -d $outdir ]; then
+			mkdir -p $outdir
+		fi
+		outfilename=$outdir/$cssname
+		lessc $filename $outfilename ) &
 	pid=$!
 	pids+=( "$pid" )
 	lessfiles+=( "$filename" )
 done < <(find src -name "*.less")
-
+failures=0
 for (( i=0; i<$(( ${#pids[*]} )); i++ )); do
 	pid="${pids[i]}"
 	lessfile="${lessfiles[i]}"
 	wait $pid
-	if [ $? -ne "0" ]; then
+	if [ $? -ne 0 ]; then
 		let failures=$failures+1
 		printf "\x1b[31mFailed to compile file $lessfile\x1b[39m\n"
 	else
 		printf "\x1b[32mCompiled $lessfile successfully\x1b[39m\n"
 	fi
-	let index=$index+1
 done
-if [ $failures -ne "0" ]; then
+if [ $failures -ne 0 ]; then
 	printf "\x1b[31;1mFailed to compile $failures less files\x1b[39;22m\n"
 	exit 1
 fi
