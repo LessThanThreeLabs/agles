9e1358978cf179f1167e46e8d01c598fc2c59538
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-01-31 20:16:15 -0800

    Cleaning up web compile scripts a bit

--- ci/web/front/compile.sh
+++ ci/web/front/compile.sh
@@ -5,13 +5,13 @@ icedPid=$!
 lessRc=$?
 wait $icedPid
 icedRc=$?
-if [ $lessRc -ne "0" -a $icedRc -ne "0" ]; then
+if [ $lessRc -ne 0 ] && [ $icedRc -ne 0 ]; then
 	printf "\x1b[31;1mFailed to compile less and coffeescript\x1b[39;22m\n"
 	exit $lessRc
-elif [ $lessRc -ne "0" ]; then
+elif [ $lessRc -ne 0 ]; then
 	printf "\x1b[31;1mFailed to compile less\x1b[39;22m\n"
 	exit $lessRc
-elif [ $icedRc -ne "0" ]; then
+elif [ $icedRc -ne 0 ]; then
 	printf "\x1b[31;1mFailed to compile coffeescript\x1b[39;22m\n"
 	exit $icedRc
 else
