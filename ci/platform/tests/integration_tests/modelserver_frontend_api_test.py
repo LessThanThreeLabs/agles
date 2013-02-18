cdf785dd35236ad80a7f855cdfd6cd9e35427c0a
Jordan Potter <jordannpotter@gmail.com>
2013-02-18 15:37:51 -0800

    Adds binascii

--- ci/platform/tests/integration_tests/modelserver_frontend_api_test.py
+++ ci/platform/tests/integration_tests/modelserver_frontend_api_test.py
@@ -37,6 +37,7 @@ buildTestLines[] : dict = getTestOutput(userId, buildId)
 <more to be added>
 '''
 import database
+import binascii
 
 from hashlib import sha512
 
@@ -73,7 +74,7 @@ class ModelServerFrontEndApiTest(BaseIntegrationTest, ModelServerTestMixin, Rabb
 ###################
 
 	def _create_user(self):
-		self.password_hash = binascii.b2a_base64(sha512().digest())
+		self.password_hash = binascii.b2a_base64(sha512().digest())[0:-1]
 		self.user_info = {'email': self.EMAIL, 'first_name': 'bob', 'last_name': 'barker',
 			'password_hash': self.password_hash, 'salt': 'Sodium Chloride.'}
 		with ModelServer.rpc_connect("users", "create") as conn:
