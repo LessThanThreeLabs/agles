2fc14db8db97136a6cc475f7f37fb25c30126c24
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-02-19 11:58:42 -0800

    Add more settings for openstack to be usable internally

--- ci/platform/settings/openstack.py
+++ ci/platform/settings/openstack.py
@@ -7,14 +7,15 @@ class OpenstackSettings(Settings):
 			version="2",
 			username="lessthanthree",
 			api_key="8eb41c1eaab4e10b3e7603d7630b26c3",
+			project_name="lessthanthree",
 			auth_url="https://identity.api.rackspacecloud.com/v2.0/",
 			auth_system="rackspace",
 			region_name="DFW",
 			vm_image_name_prefix="precise64_box_")
 		self.add_values(
-			credentials=([str(self.version), str(self.username), str(self.api_key), str(self.username)], dict(
+			credentials=([str(self.version), str(self.username), str(self.api_key), str(self.project_name)], dict(
 				auth_url=str(self.auth_url), auth_system=str(self.auth_system),
-				region_name=str(self.region_name))),
+				region_name=str(self.region_name) if self.region_name else None)),
 			image_filter=lambda cls, image: image.name.startswith(OpenstackSettings.vm_image_name_prefix))
 
 OpenstackSettings.initialize()
