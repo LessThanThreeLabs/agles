706cc8203227695296bbbc6a505065f11793cb5d
Jordan Potter <jordannpotter@gmail.com>
2013-02-25 17:24:49 -0800

    Makes the data generator much faster

--- ci/platform/bin/data_generator.py
+++ ci/platform/bin/data_generator.py
@@ -18,9 +18,11 @@ USER_EMAIL = 'user@user.com'
 USER_PASSWORD = 'user123'
 
 
-NUM_REPOS = 3
+NUM_REPOS = 1
+NUM_REPO_STORES = 1
+NUM_USERS = 1
+NUM_COMMITS = 40
 REPOSITORIES_PATH = 'repos'
-VALID_STATUSES = ['queued', 'running', 'passed', 'failed']
 
 
 class SchemaDataGenerator(object):
@@ -55,11 +57,11 @@ class SchemaDataGenerator(object):
 		with ConnectionFactory.get_sql_connection() as conn:
 			repos = dict()
 			repo_ids = []
-			for repostore in range(random.randint(1, 3)):
+			for repostore in range(NUM_REPO_STORES):
 				ins_repostore = schema.repostore.insert().values(repositories_path=REPOSITORIES_PATH, host_name=hashlib.sha1(str(repostore)).hexdigest())
 				repostore_id = conn.execute(ins_repostore).inserted_primary_key[0]
 
-				for repo in range(random.randint(1, NUM_REPOS)):
+				for repo in range(NUM_REPOS):
 					ins_repo = schema.repo.insert().values(name="repo_%d" % repo, uri="uri_%d_%d" % (repostore, repo),
 						repostore_id=repostore_id, forward_url="bogusurl", publickey="somepublickey",
 						privatekey="someprivatekey", created=int(time.time()))
@@ -67,13 +69,13 @@ class SchemaDataGenerator(object):
 					repos[repo_id] = 0
 					repo_ids.append(repo_id)
 
-			for user in range(random.randint(1, 10)):
+			for user in range(NUM_USERS):
 				ins_user = schema.user.insert().values(first_name="firstname_%d" % user, last_name="lastname_%d" % user, email="%d@b.com" % user,
 					password_hash=binascii.b2a_base64(hashlib.sha512(SALT + USER_PASSWORD.encode('utf8')).digest())[0:-1], salt=SALT)
 				user_id = conn.execute(ins_user).inserted_primary_key[0]
 
 				repo_id = random.choice(repo_ids)
-				for commit in range(random.randint(1, 20)):
+				for commit in range(NUM_COMMITS):
 					repo_id = random.choice(repos.keys())
 					repo_id_query = schema.repo.select().where(schema.repo.c.id == repo_id)
 					repo_id = conn.execute(repo_id_query).first()[schema.repo.c.id]
@@ -82,11 +84,11 @@ class SchemaDataGenerator(object):
 						message="message_%d" % commit, timestamp=random.randint(1, int(time.time())))
 					commit_id = conn.execute(ins_commit).inserted_primary_key[0]
 					ins_change = schema.change.insert().values(commit_id=commit_id, repo_id=repo_id, merge_target="target_%d" % commit,
-						number=repos[repo_id], status=random.choice(VALID_STATUSES),
+						number=repos[repo_id], status=self.get_random_commit_status(),
 						start_time=int(time.time()) + random.randint(-10000, 10000),
 						end_time=int(time.time()) + random.randint(10000, 12000))
 					change_id = conn.execute(ins_change).inserted_primary_key[0]
-					ins_build = schema.build.insert().values(change_id=change_id, repo_id=repo_id, is_primary=True, status=random.choice(VALID_STATUSES),
+					ins_build = schema.build.insert().values(change_id=change_id, repo_id=repo_id, is_primary=True, status=self.get_random_commit_status(),
 						start_time=int(time.time()) + random.randint(-10000, 10000),
 						end_time=int(time.time()) + random.randint(10000, 12000))
 					build_id = conn.execute(ins_build).inserted_primary_key[0]
@@ -104,10 +106,16 @@ class SchemaDataGenerator(object):
 						console_id = conn.execute(ins_console).inserted_primary_key[0]
 						self.generate_console_output(conn, console_id)
 
+	def get_random_commit_status(self):
+		if random.randint(0, 100) > 25:
+			return 'passed'
+		else:
+			return 'failed'
+
 	def generate_console_output(self, sqlconn, console_id):
 		console_output = schema.console_output
 
-		for line_num in range(1, random.randint(20, 500)):
+		for line_num in range(1, random.randint(20, 200)):
 			output = ''.join(random.choice(string.ascii_letters + string.digits + ' ') for x in range(random.randint(1, 100)))
 			ins = console_output.insert().values(
 				build_console_id=console_id,
