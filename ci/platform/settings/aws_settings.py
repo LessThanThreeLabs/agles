e31efaec4a481c0b8e7428a1935f13a3e5ba724c
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-03-06 22:32:04 -0800

    Add aws settings file

---
+++ ci/platform/settings/aws_settings.py
@@ -0,0 +1,9 @@
+from settings import Settings
+
+
+class AwsSettings(Settings):
+	def __init__(self):
+		super(AwsSettings, self).__init__(
+			region="us-west-2",
+			aws_access_key_id="AKIAJMQW32VH2MIGJ3UQ",
+			aws_secret_access_key="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")
