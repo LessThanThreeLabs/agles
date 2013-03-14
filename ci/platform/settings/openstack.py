7e0315df8e132b2d2f602a4b264b9c1cb32f39c8
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-03-13 18:27:14 -0700

    Remove openstack default settings

--- ci/platform/settings/openstack.py
+++ ci/platform/settings/openstack.py
@@ -5,12 +5,12 @@ class OpenstackSettings(Settings):
 	def __init__(self):
 		super(OpenstackSettings, self).__init__(
 			version="2",
-			username="lessthanthree",
-			api_key="8eb41c1eaab4e10b3e7603d7630b26c3",
-			project_name="lessthanthree",
-			auth_url="https://identity.api.rackspacecloud.com/v2.0/",
-			auth_system="rackspace",
-			region_name="DFW",
+			username="",
+			api_key="",
+			project_name="",
+			auth_url="",
+			auth_system="",
+			region_name="",
 			vm_image_name_prefix="precise64_box_")
 		self.add_values(
 			credentials=([str(self.version), str(self.username), str(self.api_key), str(self.project_name)], dict(
