cdc629d42564a0eae5315fe0ccfee56e4b05925c
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-03-13 19:00:41 -0700

    Remove stale references to RepositoryPermissions from ReposUpdateHandler

--- ci/platform/model_server/repos/update_handler.py
+++ ci/platform/model_server/repos/update_handler.py
@@ -12,7 +12,7 @@ class ReposUpdateHandler(ModelServerRpcHandler):
 
 	def update_repostore(self, repostore_id, ip_address, root_dir, num_repos):
 		repostore = database.schema.repostore
-		query = repostore.select().where(repostore.c.id==repostore_id)
+		query = repostore.select().where(repostore.c.id == repostore_id)
 		with ConnectionFactory.get_sql_connection() as sqlconn:
 			row = sqlconn.execute(query).first()
 			assert row is not None
@@ -24,7 +24,7 @@ class ReposUpdateHandler(ModelServerRpcHandler):
 
 	def _get_repostore_id_and_repo_name(self, repo_id):
 		schema = database.schema
-		query = schema.repo.select().where(schema.repo.c.id==repo_id)
+		query = schema.repo.select().where(schema.repo.c.id == repo_id)
 		with ConnectionFactory.get_sql_connection() as sqlconn:
 			row = sqlconn.execute(query).first()
 			assert row is not None
@@ -33,21 +33,11 @@ class ReposUpdateHandler(ModelServerRpcHandler):
 		return dict(repostore_id=repostore_id, repo_name=repo_name)
 
 	def force_push(self, repo_id, user_id, from_target, to_target):
-		permission = database.schema.permission
-		row = self._get_repo_permissions(user_id, repo_id)
-		if not row or not RepositoryPermissions.has_permissions(row[permission.c.permissions], RepositoryPermissions.RWA):
-			raise InvalidPermissionsError("user_id: %d, repo_id: %d" % (user_id, repo_id))
-
 		info = self._get_repostore_id_and_repo_name(repo_id)
 		manager = repo.store.DistributedLoadBalancingRemoteRepositoryManager(ConnectionFactory.get_redis_connection())
 		manager.push_force(info['repostore_id'], repo_id, info['repo_name'], from_target, to_target)
 
 	def force_delete(self, repo_id, user_id, target):
-		permission = database.schema.permission
-		row = self._get_repo_permissions(user_id, repo_id)
-		if not row or not RepositoryPermissions.has_permissions(row[permission.c.permissions], RepositoryPermissions.RWA):
-			raise InvalidPermissionsError("user_id: %d, repo_id: %d" % (user_id, repo_id))
-
 		info = self._get_repostore_id_and_repo_name(repo_id)
 
 		manager = repo.store.DistributedLoadBalancingRemoteRepositoryManager(ConnectionFactory.get_redis_connection())
@@ -57,7 +47,7 @@ class ReposUpdateHandler(ModelServerRpcHandler):
 		repo = database.schema.repo
 
 		# A repo should already exist
-		update = repo.update().where(repo.c.id==repo_id).values(forward_url=forward_url)
+		update = repo.update().where(repo.c.id == repo_id).values(forward_url=forward_url)
 		with ConnectionFactory.get_sql_connection() as sqlconn:
 				sqlconn.execute(update)
 		self.publish_event("repos", repo_id, "forward url updated", forward_url=forward_url)
