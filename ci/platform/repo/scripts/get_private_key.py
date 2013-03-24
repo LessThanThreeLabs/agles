29988476dda89f96f2706aaced8c312f9fa9b136
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-03-23 18:09:51 -0700

    Add support for GIT_SSH_TIMEOUT variable for repostore git

--- ci/platform/repo/scripts/get_private_key.py
+++ ci/platform/repo/scripts/get_private_key.py
@@ -7,7 +7,7 @@ def main():
 	repo_path = os.getcwd()
 	main_repo_path = repo_path[:repo_path.rfind('.git')] + '.git'
 	private_key = main_repo_path + '.id_rsa'
-	os.execlp('timeout', 'timeout', '120',
+	os.execlp('timeout', 'timeout', str(os.environ['GIT_SSH_TIMEOUT']),
 		'ssh', '-i', private_key,
 		'-oStrictHostKeyChecking=no',
 		'-oUserKnownHostsFile=/dev/null',
