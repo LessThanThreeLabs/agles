05041a8c01d825d9128cfba8e6dfbecac03520e1
Jordan Potter <jordannpotter@gmail.com>
2013-03-25 15:14:13 -0700

    Corrects scripts

--- company/website/scripts/exportToWebsite.sh
+++ company/website/scripts/exportToWebsite.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+cd $(dirname $0)
 
 cd ..
 rm -rf front/js/src
 rm -rf front/css/src
 rm -rf back/js
+rm -rf back/node_modules
+
 front/compile.sh
 back/compile.sh
-rm -rf back/node_modules
+
 scp -r front web@166.78.156.186:front
 scp -r back web@166.78.156.186:back
