d3b7e255c2b5b1807e45441237436d3f620f8dd7
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-03-25 19:50:11 -0700

    Make git ssh use the timeout command, because fuck OSX

--- ci/platform/repo/scripts/get_private_key.py
+++ ci/platform/repo/scripts/get_private_key.py
@@ -2,25 +2,20 @@
 import os
 import sys
 
-from subprocess import Popen
-
 
 def main():
 	repo_path = os.getcwd()
 	main_repo_path = repo_path[:repo_path.rfind('.git')] + '.git'
 	private_key = main_repo_path + '.id_rsa'
 
-	spawn_timeout_daemon(os.getpid(), float(os.environ.get('GIT_SSH_TIMEOUT', 120)))
+	timeout = os.environ.get('GIT_SSH_TIMEOUT', 120)
 
-	os.execlp('ssh', 'ssh', '-q',
+	os.execlp('timeout', 'timeout', str(timeout),
+		'ssh', '-q',
 		'-i', private_key,
 		'-oStrictHostKeyChecking=no',
 		'-oUserKnownHostsFile=/dev/null',
 		*sys.argv[1:])
-
-
-def spawn_timeout_daemon(pid, timeout):
-	Popen(['sh', '-c', 'sleep %f && kill %d' % (timeout, pid)])
 
 
 if __name__ == '__main__':
