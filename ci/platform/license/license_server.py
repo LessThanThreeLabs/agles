f1bfd3c11405428377816fe7aa67a27d1effd56d
Jonathan Chu <jchonphoenix@gmail.com>
2013-03-28 00:08:16 -0700

    Finishing base license server + tests

--- ci/platform/license/license_server.py
+++ ci/platform/license/license_server.py
@@ -1,12 +1,13 @@
 import time
 
-
 import requests
-import model_server
+
+from settings.deployment import DeploymentSettings
+
 
 LICENSE_VERIFICATION_URL = 'https://koalitycode.com/license/check'
 MAX_FAILURES = 12
-HOUR = 7200
+HOUR = 3600
 
 
 class LicenseServer(object):
@@ -16,26 +17,33 @@ class LicenseServer(object):
 	def run(self):
 		while True:
 			try:
-				with model_server.rpc_connect("resource", "read") as client:
-					info = client.get_license_info()
-					valid = self._verify_key(info['key'], info['server_id'])
-					if valid:
-						client.reset_license_check_failures()
-					else:
-						failures = client.license_check_failed()
-						if failures > MAX_FAILURES:
-							client.deactivate_license()
+				key = DeploymentSettings.license
+				server_id = DeploymentSettings.server_id
+				valid = self.verify_key(key, server_id)
+
+				if valid:
+					self.reset_license_check_failures()
+				else:
+					self.license_check_failed()
 			except:
-				with model_server.rpc_connect("resource", "read") as client:
-					failures = client.license_check_failed()
-					if failures > MAX_FAILURES:
-						client.deactivate_license()
+				self.license_check_failed()
 
 			time.sleep(2*HOUR)
 
-	def _verify_key(self, key, server_id):
+	def verify_key(self, key, server_id):
 		verification_data = {'key': key, 'server_id': server_id}
 		response = requests.post(LICENSE_VERIFICATION_URL, data=verification_data)
 		if response.status_code != requests.codes.ok:
 			return False
 		return response.text.lower() == 'true'
+
+	def reset_license_check_failures(self):
+		DeploymentSettings.license_validation_failures = 0
+		DeploymentSettings.active = True
+
+	def license_check_failed(self):
+		failures = DeploymentSettings.license_validation_failures + 1
+		DeploymentSettings.license_validation_failures = failures
+
+		if failures > MAX_FAILURES:
+			DeploymentSettings.active = False
---
+++ ci/platform/tests/integration_tests/license_server_tests.py
@@ -0,0 +1,40 @@
+from nose.tools import *
+from license.license_server import LicenseServer, MAX_FAILURES
+from settings.deployment import DeploymentSettings
+from util.test import BaseIntegrationTest
+from util.test.mixins import ModelServerTestMixin, RabbitMixin
+
+
+class LicenseServerTest(BaseIntegrationTest, ModelServerTestMixin, RabbitMixin):
+	def setUp(self):
+		super(LicenseServerTest, self).setUp()
+		self._purge_queues()
+		self._start_model_server()
+
+	def tearDown(self):
+		super(LicenseServerTest, self).tearDown()
+		self._stop_model_server()
+		self._purge_queues()
+
+	def test_reset_license_check_failures(self):
+		serve = LicenseServer()
+		for i in range(MAX_FAILURES + 1):
+			assert_equals(i, DeploymentSettings.license_validation_failures)
+			serve.license_check_failed()
+
+		assert_equals(False, DeploymentSettings.active)
+		assert_true(DeploymentSettings.license_validation_failures > 0)
+
+		serve.reset_license_check_failures()
+		assert_equals(0, DeploymentSettings.license_validation_failures)
+		assert_equals(True, DeploymentSettings.active)
+
+	def test_license_check_failed(self):
+		serve = LicenseServer()
+		DeploymentSettings.active = True
+		assert_equals(True, DeploymentSettings.active)
+		for i in range(MAX_FAILURES + 1):
+			assert_equals(i, DeploymentSettings.license_validation_failures)
+			serve.license_check_failed()
+
+		assert_equals(False, DeploymentSettings.active)
