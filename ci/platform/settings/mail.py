72751381c9c3643357b03c3db8da64beceffc530
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-04-01 21:53:35 -0700

    Route mail through koalitycode instead of getkoality

--- ci/platform/settings/mail.py
+++ ci/platform/settings/mail.py
@@ -4,6 +4,6 @@ from database_backed_settings import DatabaseBackedSettings
 class MailSettings(DatabaseBackedSettings):
 	def __init__(self):
 		super(MailSettings, self).__init__(
-			api_url='https://api.mailgun.net/v2/getkoality.com/messages',
+			api_url='https://api.mailgun.net/v2/koalitycode.com/messages',
 			api_key='key-9e06ajco0xurqnioji-egwuwajg4jrn2',
 			test_mode=True)
