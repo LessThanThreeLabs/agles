eab397eb4fa819f3f61a6f99883d470c0f0dc6ee
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-04-04 16:13:53 -0700

    Change repo caching path in snapshot to /repositories/cached

--- ci/platform/bin/ec2_snapshotter.py
+++ ci/platform/bin/ec2_snapshotter.py
@@ -15,11 +15,11 @@ def main():
 	vm_class = Ec2Vm
 	newest_global_image = vm_class.get_newest_global_image()
 	newest_image_version = vm_class.get_image_version(vm_class.get_newest_image())
-	cache_version = newest_image_version[0], newest_image_version[1] + 1
-	instance_name = 'koality_cache_%d_%d' % cache_version
+	snapshot_version = newest_image_version[0], newest_image_version[1] + 1
+	instance_name = 'koality_snapshot_%d_%d' % snapshot_version
 
 	print 'Creating new instance named "%s" based on image "%s"' % (instance_name, newest_global_image.name)
-	virtual_machine = vm_class.from_directory_or_construct("/tmp/koality/cache/%d_%d" % cache_version, instance_name, newest_global_image.id)
+	virtual_machine = vm_class.from_directory_or_construct("/tmp/koality/cached/%d_%d" % snapshot_version, instance_name, newest_global_image.id)
 	virtual_machine.wait_until_ready()
 
 	with model_server.rpc_connect('repos', 'read') as model_rpc:
@@ -37,19 +37,19 @@ def main():
 	primary_branch = branch_counter.most_common(1)[0][0]
 	print 'Primary branch is "%s"' % primary_branch
 
-	virtual_machine.ssh_call('sudo mkdir -p /repositories/cache && sudo chown lt3:lt3 /repositories/cache')
+	virtual_machine.ssh_call('sudo mkdir -p /repositories/cached && sudo chown lt3:lt3 /repositories/cached')
 	uri_translator = RepositoryUriTranslator()
 	for repository in repositories:
 		print 'Cloning repository "%s"' % repository['name']
 		virtual_machine.remote_clone(uri_translator.translate(repository['uri']))
-		virtual_machine.ssh_call('rm -rf /repositories/cache/%s; mv source /repositories/cache/%s' % (repository['name'], repository['name']))
+		virtual_machine.ssh_call('rm -rf /repositories/cached/%s; mv source /repositories/cached/%s' % (repository['name'], repository['name']))
 
 	print 'Provisioning for repository "%s" on branch "%s"' % (primary_repository['name'], primary_branch)
-	virtual_machine.ssh_call('mv /repositories/cache/%s source' % primary_repository['name'])
+	virtual_machine.ssh_call('mv /repositories/cached/%s source' % primary_repository['name'])
 	virtual_machine.provision(primary_repository['privatekey'])
-	virtual_machine.ssh_call('mv source /repositories/cache/%s' % primary_repository['name'])
+	virtual_machine.ssh_call('mv source /repositories/cached/%s' % primary_repository['name'])
 
-	new_image_name = '%s%d_%d' % (AwsSettings.vm_image_name_prefix, cache_version[0], cache_version[1])
+	new_image_name = '%s%d_%d' % (AwsSettings.vm_image_name_prefix, snapshot_version[0], snapshot_version[1])
 	print 'Saving instance as AMI "%s"' % new_image_name
 	virtual_machine.create_image(new_image_name)
 
