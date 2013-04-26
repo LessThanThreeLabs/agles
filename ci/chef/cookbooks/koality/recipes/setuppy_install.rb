609167124f54803ce3dd3999a3a258d756281b0d
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-04-25 19:00:45 -0700

    Fix chef setuppy_install command

--- ci/chef/cookbooks/koality/recipes/setuppy_install.rb
+++ ci/chef/cookbooks/koality/recipes/setuppy_install.rb
@@ -1,5 +1,5 @@
 execute "install_koality" do
 		cwd node[:koality][:source_path][:platform]
 		user "root"
-		command "bash -c 'source /etc/koality/koalityrc && python setup.py install'"
+		command "/etc/koality/python setup.py install'"
 end
