86032a627300544cc86b8ce43a90e966407ebbb7
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-05-01 13:42:46 -0700

    Removing chef stuff from the repository
    See koality-manager repository for packaging/installing stuff
    TODO: fix or remove koality service in initial_setup.sh

--- ci/chef/client-config/client.rb
+++
@@ -1,4 +0,0 @@
-log_level        :info
-log_location     STDOUT
-chef_server_url  'https://api.opscode.com/organizations/lessthanthree'
-validation_client_name 'lessthanthree-validator'
--- ci/chef/roles/verification_box_run.rb
+++
@@ -1,9 +0,0 @@
-name "verification_box_run"
-description "Stripped down koality verification virtual box run"
-run_list(
-	"recipe[koality::copy_source]",
-	"recipe[koality::language_config]",
-	"recipe[koality::compile_config]",
-	"recipe[koality::test_config]",
-	"recipe[koality::setup_config]"
-)
