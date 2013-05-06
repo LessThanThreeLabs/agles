9035f8df3f5dcd317f2cf4a266938194f8054720
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-05-05 18:08:39 -0700

    Handle more cases when parsing verification config
    Allow compile/test/factory steps to be defined as strings, lists, or dictionaries

--- ci/platform/verification/verification_config.py
+++ ci/platform/verification/verification_config.py
@@ -8,14 +8,32 @@ class VerificationConfig(object):
 			test_commands = test_section.get('scripts')
 			test_factory_commands = test_section.get('factories')
 			compile_commands = compile_section.get('scripts')
-			self.compile_commands = [RemoteCompileCommand(command)
-				for command in compile_commands] if compile_commands else []
-			self.test_commands = [RemoteTestCommand(command)
-				for command in test_commands] if test_commands else []
-			self.test_factory_commands = [RemoteTestFactoryCommand(command)
-				for command in test_factory_commands] if test_factory_commands else []
+			self.compile_commands = self._convert(RemoteCompileCommand, compile_commands)
+			self.test_commands = self._convert(RemoteTestCommand, test_commands)
+			self.test_factory_commands = self._convert(RemoteTestFactoryCommand, test_factory_commands)
 		except:
 			self.machines = 1
 			self.compile_commands = []
 			self.test_commands = [RemoteErrorCommand("parse error", "Could not parse your koality.yml file.\nPlease verify that it is valid yaml and matches the expected format.")]
 			self.test_factory_commands = []
+
+	def _convert(self, command_class, commands):
+		if not commands:
+			return []
+		if isinstance(commands, str):
+			return command_class(commands)
+		if isinstance(commands, list):
+			return self._convert_list(command_class, commands)
+		if isinstance(commands, dict):
+			return self._convert_dict(command_class, commands)
+		raise InvalidConfigurationException("Unexpected type found while trying to construct %ss" % command_class.__name__)
+
+	def _convert_list(self, command_class, commands):
+		return [command_class(command) for command in commands]
+
+	def _convert_dict(self, command_class, commands):
+		return self._convert_list(command_class, map(lambda entry: dict((entry,)), commands.iteritems()))
+
+
+class InvalidConfigurationException(Exception):
+	pass
