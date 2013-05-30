c71a7db6ee2a69e2b78fd86657f3de204b2d2644
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-05-29 17:46:43 -0700

    Rework upgrade script entry-point to use os nonsense
    fork, waitpid, dup2. The whole gang is here
    Make sure to dup2 all the standard file descriptors, otherwise parent termination might cause unexpected pipe/stream errors

--- ci/platform/scripts/server/upgrader.py
+++ ci/platform/scripts/server/upgrader.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
-import logging
-import multiprocessing
+import os
 import sys
-
-import util.log
-
-from upgrade.upgrader import Upgrader, HttpTarFetcher
 
 
 def main():
 	upgrade_version = sys.argv[1] if len(sys.argv) > 1 else 'latest'
 	print 'Preparing to upgrade to version %s ...' % upgrade_version
 
-	util.log.configure()
-
 	try:
-		upgrader = Upgrader(upgrade_version, HttpTarFetcher())
+		pid = os.fork()
+		if pid != 0:
+			os.waitpid(pid, 0)
+		else:
+			stdin = open('/dev/null', 'r')
+			stdout = open('/dev/null', 'a+')
+			stderr = open('/dev/null', 'a+', 0)
+			os.dup2(stdin.fileno(), sys.stdin.fileno())
+			os.dup2(stdout.fileno(), sys.stdout.fileno())
+			os.dup2(stderr.fileno(), sys.stderr.fileno())
 
-		upgrade_process = multiprocessing.Process(target=upgrader.do_upgrade)
-		upgrade_process.start()
+			from upgrade.upgrader import Upgrader, HttpTarFetcher
 
-		upgrade_process.join()
-		if upgrade_process.exitcode < 0:
-			raise Exception('Upgrade process killed by signal %d' % (-upgrade_process.exitcode))
-		elif upgrade_process.exitcode > 0:
-			raise Exception('Upgrade process exited with return code %d' % upgrade_process.exitcode)
+			upgrader = Upgrader(upgrade_version, HttpTarFetcher())
+			upgrader.do_upgrade()
 	except:
-		logger = logging.getLogger('upgrader')
-		logger.critical('Upgrade failure', exc_info=True)
+		import traceback
+		with open('/tmp/err', 'w') as f:
+			traceback.print_exc(file=f)
 
 
 if __name__ == '__main__':
