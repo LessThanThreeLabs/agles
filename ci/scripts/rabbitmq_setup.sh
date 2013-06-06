0129d47a4572ddf20012440f891348fb19f674af
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-06-06 16:31:27 -0700

    Update rabbitmq_setup script to remove guest user and update lt3 user's password
    Also is formatted in a manner that allows for easily changing the password via sed
    This allows us to give each deployment a unique randomly-generated password

--- ci/scripts/rabbitmq_setup.sh
+++ ci/scripts/rabbitmq_setup.sh
@@ -1,6 +1,13 @@
-(rabbitmqctl list_users -q | grep '^lt3') > /dev/null || rabbitmqctl add_user lt3 42f6e8eacf66b9ee7c7b0a5b6a0e1498f7c0d42f
-rabbitmqctl set_user_tags lt3 administrator
-rabbitmqctl set_permissions lt3 '.*' '.*' '.*'
-# rabbitmqctl list_users -q | while read username tags; do
-# 	[ $username == 'lt3' ] || rabbitmqctl delete_user $username
-# done
+#!/bin/bash
+username=lt3
+password=42f6e8eacf66b9ee7c7b0a5b6a0e1498f7c0d42f
+if [ "$(rabbitmqctl list_users -q | grep "^$username")" ]; then
+	rabbitmqctl change_password $username $password
+else
+	rabbitmqctl add_user $username $password
+fi
+rabbitmqctl set_user_tags $username administrator
+rabbitmqctl set_permissions $username '.*' '.*' '.*'
+if [ "$(rabbitmqctl list_users -q | grep '^guest')" ]; then
+	rabbitmqctl delete_user guest
+fi
