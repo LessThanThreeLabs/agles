ec20979b4dc24226b86342756b7f66fb27ded484
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-06-11 16:52:10 -0700

    Change upgrade request from POST to GET

--- ci/platform/upgrade/upgrader.py
+++ ci/platform/upgrade/upgrader.py
@@ -91,7 +91,7 @@ class HttpTarFetcher(TarFetcher):
 
 	def fetch_bytes(self, license_key, server_id, from_version, to_version):
 		upgrade_data = {"licenseKey": license_key, "serverId": server_id, "currentVersion": from_version, "upgradeVersion": to_version}
-		response = requests.post(self._fetch_uri, data=upgrade_data)
+		response = requests.get(self._fetch_uri, params=upgrade_data)
 		if not response.ok:
 			raise UpgradeException("Failed to download upgrade tarball. upgrade_data: %s" % upgrade_data)
 		return response.content
