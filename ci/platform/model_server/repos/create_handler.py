3b515292882a8c3f448b876a76b0333de5cfd0d2
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-08-05 20:24:16 -0700

    Properly re-raise a BadRepositorySetupError after deleting the repository

--- ci/platform/model_server/repos/create_handler.py
+++ ci/platform/model_server/repos/create_handler.py
@@ -56,10 +56,10 @@ class ReposCreateHandler(ModelServerRpcHandler):
 
 			self.publish_event_to_all("users", "repository added", repo_id=repo_id, repo_name=repo_name, forward_url=forward_url, created=current_time)
 			return repo_id
-		except repo.store.BadRepositorySetupError:
+		except repo.store.BadRepositorySetupError as e:
 			with model_server.rpc_connect('repos', 'delete') as repos_delete_handler:
 				repos_delete_handler.delete_repo(user_id, repo_id)
-			raise
+			raise e
 		except RepositoryCreateError as e:
 			error_msg = "failed to create repo: [user_id: %d, repo_name: %s]" % (user_id, repo_name)
 			self.logger.exception(error_msg)
