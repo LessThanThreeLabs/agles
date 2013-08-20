042f2e59231959239ed44ba8f7a2089136b0d687
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-08-19 18:43:16 -0700

    Some optimizations for reading/writing console output
    Hopefully make fewer writes when receiving large output chunks
    Allow for reading only a subset of the console output from the end
    Also depend on newer version of streaming executor for larger write chunks

--- ci/platform/model_server/build_consoles/read_handler.py
+++ ci/platform/model_server/build_consoles/read_handler.py
@@ -36,28 +36,18 @@ class BuildConsolesReadHandler(ModelServerRpcHandler):
 		with ConnectionFactory.get_sql_connection() as sqlconn:
 			return [to_dict(row, build_console.columns, tablename=build_console.name) for row in sqlconn.execute(query)]
 
-	def get_output_lines(self, user_id, build_console_id):
+	def get_output_lines(self, user_id, build_console_id, offset=0, num_results=1000):
 		console_output = database.schema.console_output
 		build_console = database.schema.build_console
 
-		output_query = console_output.select().where(console_output.c.build_console_id == build_console_id)
-		metadata_query = build_console.select().where(build_console.c.id == build_console_id)
+		output_query = console_output.select().where(
+			console_output.c.build_console_id == build_console_id
+		).order_by(
+			console_output.c.line_number.desc()
+		).limit(num_results).offset(offset)
 
 		with ConnectionFactory.get_sql_connection() as sqlconn:
 			return {row[console_output.c.line_number]: row[console_output.c.line] for row in sqlconn.execute(output_query)}
-
-	def get_console_output(self, user_id, build_console_id):
-		console_output = database.schema.console_output
-		build_console = database.schema.build_console
-
-		output_query = console_output.select().where(console_output.c.build_console_id == build_console_id)
-		metadata_query = build_console.select().where(build_console.c.id == build_console_id)
-
-		with ConnectionFactory.get_sql_connection() as sqlconn:
-			output = dict([(row[console_output.c.line_number], row[console_output.c.line]) for row in sqlconn.execute(output_query)])
-			console_metadata = to_dict(sqlconn.execute(metadata_query).first(), build_console.columns)
-			console_metadata[console_output.name] = output
-			return console_metadata
 
 	def can_hear_build_console_events(self, user_id, id_to_listen_to):
 		return True
