6d876fc7d5f56792b22b7cd55fec9e374b44004c
Andrey Kostov <kostov.andrey@gmail.com>
2013-08-25 15:34:22 -0700

    Add debug_instances

---
+++ ci/platform/model_server/debug_instances/create_handler.py
@@ -0,0 +1,35 @@
+import database.schema
+
+from database.engine import ConnectionFactory
+from model_server.rpc_handler import ModelServerRpcHandler
+
+
+class DebugInstancesCreateHandler(ModelServerRpcHandler):
+	def __init__(self):
+		super(DebugInstancesCreateHandler, self).__init__("debug_instances", "create")
+
+	def create_instance(self, vm_type, instance_id, pool_slot, username):
+		virtual_machine = database.schema.virtual_machine
+
+		existing_vm = virtual_machine.select().where(
+			and_(
+				virtual_machine.c.instance_id == instance_id,
+				virtual_machine.c.deleted == 0
+			)
+		)
+
+		with ConnectionFactory.get_sql_connection() as sqlconn:
+			if sqlconn.execute(existing_vm).rowcount > 0:
+				raise VirtualMachineAlreadyExistsError(instance_id)
+			ins = virtual_machine.insert().values(
+				type=vm_type,
+				instance_id=instance_id,
+				pool_slot=pool_slot,
+				username=username)
+			result = sqlconn.execute(ins)
+
+		vm_id = result.inserted_primary_key[0]
+		return vm_id
+
+class VirtualMachineAlreadyExistsError(Exception):
+	pass
---
+++ ci/platform/model_server/debug_instances/update_handler.py
@@ -0,0 +1,45 @@
+from database import schema
+from database.engine import ConnectionFactory
+from model_server.rpc_handler import ModelServerRpcHandler
+from settings.web_server import WebServerSettings
+from util.mail import sendmail
+
+LAUNCH_TEMPLATE = """%s %s,
+
+To SSH into your debug instance type the following command into your terminal:
+
+ssh verification@%s "ssh %s"
+
+-The Koality Team"""
+
+
+class DebugInstancesUpdateHandler(ModelServerRpcHandler):
+
+	def __init__(self):
+		super(DebugInstancesUpdateHandler, self).__init__("debug_instances", "update")
+
+	# TODO(andrey) Eventually there needs to be a page in the front end ui containing the currently running QA vm's.
+	def mark_qa_build_launched(self, build_id, change_id):
+		self._notify_instance_spawned(build_id, change_id)
+
+	def _get_user_row(self, change_id):
+		change = schema.change
+		user = schema.user
+		commit = schema.commit
+
+		query = change.join(commit).join(user).select(use_labels=True).where(change.c.id == change_id)
+		with ConnectionFactory.get_sql_connection() as sqlconn:
+			return sqlconn.execute(query).first()
+
+	def _notify_instance_spawned(self, build_id, change_id):
+		row = self._get_user_row(change_id)
+		user = schema.user
+
+		email = row[user.c.email]
+		first_name = row[user.c.first_name]
+		last_name = row[user.c.last_name]
+
+		subject = "Your debug instance has launched"
+		text = LAUNCH_TEMPLATE % (first_name, last_name, WebServerSettings.domain_name, build_id)
+
+		return sendmail("buildbuddy@koalitycode.com", [email], subject, text)
