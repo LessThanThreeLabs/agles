6590dee3e60f0bbfec69abf27bd2c3883c539ed9
Andrey Kostov <kostov.andrey@gmail.com>
2013-09-04 17:09:06 -0700

    Fix show-koality from bundle

--- ci/platform/util/restricted_shell.py
+++ ci/platform/util/restricted_shell.py
@@ -203,6 +203,7 @@ class RestrictedHgShell(RestrictedShell):
 		args = self.rp_new_sshargs("hg -R", requested_repo_uri, user_id)
 		os.execlp(*args)
 
+	# TODO(andrey) refactor this and cat-bundle
 	def handle_show_koality(self, requested_repo_uri, user_id, sha):
 		with model_server.rpc_connect("repos", "read") as modelserver_rpc_conn:
 			attributes = modelserver_rpc_conn.get_repo_attributes(requested_repo_uri)
@@ -213,9 +214,9 @@ class RestrictedHgShell(RestrictedShell):
 		self.verify_user_exists("git-show", user_id, attributes['repo']['id'])
 		remote_filesystem_path = os.path.join(attributes['repostore']['repositories_path'], pathgen.to_path(attributes['repo']['id'], attributes['repo']['name']))
 
-		yml_path = os.path.join(remote_filesystem_path, ".hg", "strip-backup", sha + "-koality.yml")
+		bundle_path = os.path.join(remote_filesystem_path, ".hg", "strip-backup", sha + ".hg")
 		uri = "git@%s" % attributes['repostore']['ip_address']
-		full_command = "sh -c %s" % pipes.quote("cat %s" % yml_path)
+		full_command = "sh -c %s" % pipes.quote("hg -R %s cat * -I koality.yml -I .koality.yml" % bundle_path)
 		os.execlp("ssh", "ssh", "-p", "2222", "-oStrictHostKeyChecking=no", uri, full_command)
 
 	def handle_cat_bundle(self, requested_repo_uri, user_id, sha):
