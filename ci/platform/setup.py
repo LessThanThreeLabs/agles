c594f0babe25ddb34749569bfa8c2a3205b93564
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-09-19 20:32:33 -0700

    Saddest patch version ever
    Monkey patch all of verification server
    Revert to 0.12.1 eventlet
    Bump to 0.4.1 of provisioner and streaming executor
    http://24.media.tumblr.com/tumblr_m8rvw0hyK91rwbpkjo1_400.gif

--- ci/platform/setup.py
+++ ci/platform/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="koality",
-	version="0.4.1",
+	version="0.4.2",
 	description="Production code for koality",
 	packages=find_packages(exclude=[
 		"bin",
