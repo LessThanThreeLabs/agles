b883581af67a8e76ae407cd6d30a918b825e0343
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-10-01 10:43:52 -0700

    Change default github enterprise url to be an empty string

--- ci/platform/settings/github_enterprise.py
+++ ci/platform/settings/github_enterprise.py
@@ -4,6 +4,6 @@ from database_backed_settings import DatabaseBackedSettings
 class GithubEnterpriseSettings(DatabaseBackedSettings):
 	def __init__(self):
 		super(GithubEnterpriseSettings, self).__init__(
-			github_url='github.yourcompany.com',
+			github_url='',
 			client_id='',
 			client_secret='')
