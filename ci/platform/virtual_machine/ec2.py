08bbf7954ea5187c7ff7476528e9a5be56e4b731
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-11-20 14:34:55 -0800

    Change allowed ec2 instance types list
    Adds c3 and gpu instances

--- ci/platform/virtual_machine/ec2.py
+++ ci/platform/virtual_machine/ec2.py
@@ -452,8 +452,13 @@ class SecurityGroups(object):
 
 
 class InstanceTypes(object):
-	ordered_types = ['m1.small', 'm1.medium', 'm1.large', 'm1.xlarge', 'm2.xlarge', 'm2.2xlarge', 'm2.4xlarge',
-			'm3.xlarge', 'm3.2xlarge', 'c1.medium', 'c1.xlarge', 'hi1.4xlarge', 'hs1.8xlarge']
+	ordered_types = ['m1.small', 'm1.medium', 'm1.large', 'm1.xlarge',
+			'm3.xlarge', 'm3.2xlarge',
+			'c1.medium', 'c1.xlarge',
+			'c3.large', 'c3.xlarge', 'c3.2xlarge', 'c3.4xlarge',
+			'm2.xlarge', 'm2.2xlarge', 'm2.4xlarge', 'cr1.8xlarge',
+			'g2.2xlarge', 'cg1.4xlarge',
+			'hi1.4xlarge', 'hs1.8xlarge']
 
 	@classmethod
 	def set_largest_instance_type(cls, largest_instance_type):
